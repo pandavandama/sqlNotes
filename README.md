8ed76f282dafd8ba058aaa2614a632d21d5f23aa
Константин Кифа <pandavandama@MacBook-Air-Konstantin.local>
2022-08-08 00:01:31 +0300

    update py file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sqlNotes
+
+### Create statement
+CREATE TABLE Persons (
+    PersonID int auto_increment primary key,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+### Number of rows
+select count(*) from Persons
+
+### Joins
--- main.py
+++ main.py
@@ -6,26 +6,35 @@ import mysql.connector
 from faker import Faker
 fake = Faker()
 
-
-
-# connection = mysql.connector.connect(host='localhost',database='learning',user='root')
-
-# cursor = connection.cursor()
-# cursor.execute("select * from Persons limit 10;")
-# cursor.execute("insert into Persons(LastName,FirstName,Address,City) values ('Krasivik','Mujcina','Urdak 10','Hell')")
-# cursor.execute("select count(*) from Persons")
-# connection.commit()
-# print(cursor.fetchone())
-# cursor.close()
-# connection.close()
-
 def makeConnection(database):
     return mysql.connector.connect(host='localhost',database=database,user='root')
 
-def fillTable(tableName):
+def createTable(name):
     connection = makeConnection("learning")
     cursor = connection.cursor()
-    cursor.execute("insert into {}(LastName,FirstName,Address,City) values ('{}','{}','{}','{}')".format(tableName,fake.last_name(),fake.first_name(),fake.address(),fake.city()))
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS {} (
+    PersonID int auto_increment primary key,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+    );""".format(name))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return name
+
+def fillTable(tableName,length):
+    connection = makeConnection("learning")
+    cursor = connection.cursor()
+    requestInsert = "insert into {}(LastName,FirstName,Address,City) values (%s,%s,%s,%s);".format(tableName)
+    requestValues = []
+    for i in range(length):
+        if i%1000==0:
+            print("Prepare in {}. {} from {}".format(tableName,i,length))
+        requestValues.append((fake.last_name(),fake.first_name(),fake.address(),fake.city()))
+    print("Request...")
+    cursor.executemany(requestInsert,requestValues)
     connection.commit()
     cursor.close()
     connection.close()
@@ -39,11 +48,13 @@ def getCountOfCells(tableName):
     connection.close()
     return response[0]
 
+length = 10000
+
+nameOfFirstTable = createTable("Persons")
+
+fillTable(nameOfFirstTable,length)
 
 
-print("hello")
-fillTable("Persons")
-getCountOfCells("Persons")
-
-for i in range(getCountOfCells("Persons")):
-    fillTable("Persons3")
+for i in range(10):
+    i+=1
+    fillTable(createTable(nameOfFirstTable+"{}".format(i)),length)
